f39112c5e9b737a42013ce93713815dd20638934
unikounio <bubbbble428@gmail.com>
2023-07-14 18:12:44 +0900

    とりあえずアプリが完成 - stylesheetを作成してレイアウトを調整した - XSS対策としてescape処理を行った - Not Foundのエラーハンドリングを行った - linterを通した

--- Gemfile
+++ Gemfile
@@ -5,3 +5,8 @@ source "https://rubygems.org"
 gem "sinatra"
 gem "webrick"
 gem 'sinatra-contrib'
+gem 'rubocop', require:false
+group :development do
+  gem 'rubocop-fjord', require: false
+end
+gem 'erb_lint', require: false
--- sinatra_memo.rb
+++ sinatra_memo.rb
@@ -1,12 +1,17 @@
+# frozen_string_literal: true
+
 require 'bundler/setup'
 Bundler.require(:default)
-require 'json'
 require 'sinatra/reloader'
+require 'json'
+require 'cgi/util'
 
 MEMOS_FILE = 'memos.json' # ファイルパスを変更しやすくするために定数化している
 
 def load_memos
-  JSON.parse(File.read(MEMOS_FILE)) rescue [] # 変数のスコープを最低限にするため、代入処理は含めていない
+  JSON.parse(File.read(MEMOS_FILE)) # 変数のスコープを最低限にするため、代入処理は含めていない
+rescue StandardError
+  []
 end
 
 def save_memos(memos)
@@ -16,53 +21,52 @@ end
 def assign_memos_element(index)
   memos = load_memos[index.to_i]
   @index = index
-  @memo_title = memos["title"]
-  @memo_body = memos["body"]
+  @memo_title = memos['title']
+  @memo_body = memos['body']
 end
 
-# トップページ
 get '/' do
   @memos = load_memos
-  erb :top, layout: :layout
+  erb :top
 end
 
-# 新規作成
 get '/new' do
-  erb :new, layout: :layout
+  erb :new
 end
 
 post '/new' do
   memos = load_memos
-  memo = { title: params[:title], body: params[:body] }
+  memo = { title: CGI.escapeHTML(params[:title]), body: CGI.escapeHTML(params[:body]) }
   memos << memo
   save_memos(memos)
   redirect to('/')
 end
 
-# メモ閲覧
 get '/show/*' do |index|
   assign_memos_element(index)
-  erb :show, layout: :layout
+  erb :show
 end
 
-# メモ編集
 get '/edit/*' do |index|
   assign_memos_element(index)
-  erb :edit, layout: :layout
+  erb :edit
 end
 
 patch '/edit/*' do |index|
   memos = load_memos
-  memo = { title: params[:title], body: params[:body] }
+  memo = { title: CGI.escapeHTML(params[:title]), body: CGI.escapeHTML(params[:body]) }
   memos[index.to_i].replace(memo)
   save_memos(memos)
   redirect to("/show/#{index}")
 end
 
-# メモ削除
 delete '/delete/*' do |index|
   memos = load_memos
   memos.delete_at(index.to_i)
   save_memos(memos)
   redirect to('/')
+end
+
+not_found do
+  erb :not_found
 end
